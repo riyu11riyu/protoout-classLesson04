2ca4b4bb73a535a82316d82c9096134dddfa0652
riyu <160996802+riyu11riyu@users.noreply.github.com>
2026-03-04 12:27:24 +0000

    LINEBot

--- sendline/app.js
+++ sendline/app.js
@@ -3,7 +3,7 @@
 const line = require('@line/bot-sdk');
 
 const config = {
-    channelSecret: '36887f5dbdd8006bf900bd1d5c1d9506',
+    channelSecret: '',
     channelAccessToken: 'Flnz8PKi7L8BZUO4WHNFkFVqIJCnUUHNj5/PV2SoF8C2lZ4QSUOOHR+TPt8qm9lwTrPvXznweJewgDVCKeCHiScqiu73tHhD+1w1GamTdhJMHLW9BYdMGblEQxiMYQkm6tvOH5dp52zvDIQfeQeY5wdB04t89/1O/w1cDnyilFU='
 };
 const client = new line.messagingApi.MessagingApiClient(config);
@@ -12,7 +12,7 @@ const main = async () => {
 
     const messages = [{
         type: 'text',
-        text: 'やっほー！'
+        text: 'うい！'
     }];
 
     try {
